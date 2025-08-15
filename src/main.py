30e9be5fe4d1aba334bd349d34f731bb5fe03697
zyaeger <zachyaeger@gmail.com>
2025-08-15 16:49:08 -0700

    add basic API implementation
    todo
    
    black

--- src/main.py
+++ src/main.py
@@ -1,5 +1,32 @@
+# pylint: disable=fixme
+import os
+from pprint import pprint
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.environ.get("GAMEBRAIN_API_KEY")
+
+
+# TODO: Add click for cli args (search terms, IDs, Platforms)
 def main():
-    return True
+    url = "https://api.gamebrain.co/v1/games/1273796"
+
+    headers = {"x-api-key": API_KEY}
+
+    try:
+        response = requests.get(url, headers=headers, timeout=None)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(e)
+
+    resp_json = response.json()
+    pprint(resp_json)
+    pprint(f"Request Token Usage: {response.headers['X-API-Quota-Request']}")
+    pprint(f"Today's Total Token Usage: {response.headers['X-API-Quota-Used']}")
+    pprint(f"Today's Remaining Tokens: {response.headers['X-API-Quota-Left']}")
+    return resp_json.get("name")
 
 
 if __name__ == "__main__":
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -3,10 +3,12 @@ import unittest
 from src import main
 
 
+# mock requests out of tests
 class DummyTest(unittest.TestCase):
     def test_dummy(self):
         dummy = main.main()
-        self.assertTrue(dummy)
+        expected = "Kingdom Come: Deliverance II"
+        self.assertEqual(dummy, expected)
 
 
 if __name__ == "__main__":
