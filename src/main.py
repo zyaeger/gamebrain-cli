30c205c5a5c6a073abe33a41f64c384d5c1b54ee
zyaeger <zachyaeger@gmail.com>
2025-08-17 17:48:43 -0700

    implement click for cli

--- src/main.py
+++ src/main.py
@@ -2,17 +2,28 @@
 import os
 from pprint import pprint
 
+import click
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 API_KEY = os.environ.get("GAMEBRAIN_API_KEY")
+BASE_URL = "https://api.gamebrain.co/v1"
 
 
-# TODO: Add click for cli args (search terms, IDs, Platforms)
-def main():
-    url = "https://api.gamebrain.co/v1/games/1273796"
+@click.group()
+def gamebrain():
+    pass
 
+
+@gamebrain.command("game-detail")
+@click.argument("game-id")
+def game_detail(game_id: str) -> dict | str:
+    """
+    Takes a Gamebrain game ID and runs a request to GET information on the Game
+    """
+    game_id = int(str(game_id))
+    url = os.path.join(BASE_URL, f"games/{game_id}")
     headers = {"x-api-key": API_KEY}
 
     try:
@@ -30,5 +41,40 @@ def main():
     return resp_json.get("name")
 
 
+@gamebrain.command("search")
+@click.argument("query")
+# Add options fo filter and sort
+def search(query: str) -> list:
+    """
+    Takes a search query and gets the first 10 games returned with their metadata
+    """
+    url = os.path.join(BASE_URL, "games")
+    # Add pagination and filters
+    click.echo(f"Querying {url} for {query}")
+    return list[query]
+
+
+@gamebrain.command("suggest")
+@click.argument("query")
+def suggest(query: str) -> list:
+    """
+    Takes and incomplete query and returns a list of similar titles
+    """
+    url = os.path.join(BASE_URL, "games/suggestions")
+    click.echo(f"Querying {url} for {query}")
+    return [query]
+
+
+@gamebrain.command("similar")
+@click.argument("game-id")
+def similar(game_id: str) -> list:
+    """
+    Takes a game_id and returns 10 games considered similar to the provided ID
+    """
+    url = os.path.join(BASE_URL, f"games/{game_id}/similar")
+    click.echo(f"Querying {url} for similar games")
+    return [game_id]
+
+
 if __name__ == "__main__":
-    main()
+    print("Querying Gamebrain API...")
